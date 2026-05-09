593d65ddebf4ca8fe11c1aa64ce6522cb1e50041
santoshgtm95 <santosh.gtm6@gmail.com>
2026-05-09 09:18:36 +0700

    feat: initialize API project with JWT authentication, EF Core, and database seeding

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import type { LoginRequest, LoginResponse, Product, CreateProductDto, Sale, CreateSaleDto, DashboardStats, User, CreateUserDto, UpdateUserDto, Role, ProcessingRecord, CreateProcessingRecordDto, Worker, Warehouse, CreateWarehouseDto, UpdateWarehouseDto, PurificationProcess, CreatePurificationProcessDto, AvailableCategory, Purifier, CreatePurifierDto, UpdatePurifierDto, PurifiedRecord } from '../types';
 
-const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:5159/api';
+const API_BASE_URL ='http://akzapi.runasp.net/api';
 
 const api = axios.create({
     baseURL: API_BASE_URL,
